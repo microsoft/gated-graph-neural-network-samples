f714619bb4a52ed238416c4d616ef2daa3ceccbe
Alexander Gaunt <t-algaun@microsoft.com>
2018-02-01 13:56:41 +0000

    fix(dense): add node masks (+ give example test time code)

--- chem_tensorflow_dense.py
+++ chem_tensorflow_dense.py
@@ -20,6 +20,7 @@ import numpy as np
 import tensorflow as tf
 import sys, traceback
 import pdb
+import json
 
 from chem_tensorflow import ChemModel
 from utils import glorot_init
@@ -56,10 +57,12 @@ class DenseGGNNChemModel(ChemModel):
         self.placeholders['initial_node_representation'] = tf.placeholder(tf.float32,
                                                                           [None, None, self.params['hidden_size']],
                                                                           name='node_features')
+        self.placeholders['node_mask'] = tf.placeholder(tf.float32, [None, None], name='node_mask')
         self.placeholders['num_vertices'] = tf.placeholder(tf.int32, ())
         self.placeholders['adjacency_matrix'] = tf.placeholder(tf.float32,
                                                                [None, self.num_edge_types, None, None])  # [b x e x v x v]
         self.__adjacency_matrix = tf.transpose(self.placeholders['adjacency_matrix'], [1, 0, 2, 3])  # [e x b x v x v]
+
 
         # weights
         self.weights['edge_weights'] = tf.Variable(glorot_init([self.num_edge_types, h_dim, h_dim]))
@@ -76,11 +79,6 @@ class DenseGGNNChemModel(ChemModel):
         h_dim = self.params['hidden_size']
         h = self.placeholders['initial_node_representation']  # [b x v x h]
         h = tf.reshape(h, [-1, h_dim])
-
-        #biases = []
-        #for a in tf.unstack(self.__adjacency_matrix, axis=0):
-        #    summed_a = tf.reshape(tf.reduce_sum(a, axis=-1), [-1, 1])  # [b*v x 1]
-        #    biases.append(tf.matmul(summed_a, self.weights['edge_biases'][0]))  # [b*v x h]
 
         # precompute edge biases
         if self.params['use_edge_bias']:
@@ -115,22 +113,26 @@ class DenseGGNNChemModel(ChemModel):
         gated_outputs = tf.nn.sigmoid(regression_gate(gate_input)) * regression_transform(last_h)        # [(b*v) x 1]
         gated_outputs = tf.reshape(gated_outputs, [-1, self.placeholders['num_vertices']])               # [b x v]
         output = tf.reduce_sum(gated_outputs, axis = 1)                                                  # [b]
+        self.output = output
         return output
 
     # ----- Data preprocessing and chunking into minibatches:
-    def process_raw_graphs(self, raw_data: Sequence[Any], is_training_data: bool) -> Any:
-        bucket_sizes = np.array(list(range(4, 28, 2)) + [29])
+    def process_raw_graphs(self, raw_data: Sequence[Any], is_training_data: bool, bucket_sizes=None) -> Any:
+        if bucket_sizes is None:
+            bucket_sizes = np.array(list(range(4, 28, 2)) + [29])
         bucketed = defaultdict(list)
         x_dim = len(raw_data[0]["node_features"][0])
         for d in raw_data:
             chosen_bucket_idx = np.argmax(bucket_sizes > max([v for e in d['graph']
                                                                 for v in [e[0], e[2]]]))
             chosen_bucket_size = bucket_sizes[chosen_bucket_idx]
+            n_active_nodes = len(d["node_features"])
             bucketed[chosen_bucket_idx].append({
                 'adj_mat': graph_to_adj_mat(d['graph'], chosen_bucket_size, self.num_edge_types, self.params['tie_fwd_bkwd']),
                 'init': d["node_features"] + [[0 for _ in range(x_dim)] for __ in
-                                              range(chosen_bucket_size - len(d["node_features"]))],
+                                              range(chosen_bucket_size - n_active_nodes)],
                 'labels': [d["targets"][task_id][0] for task_id in self.params['task_ids']],
+                'mask': [1. for _ in range(n_active_nodes) ] + [0. for _ in range(chosen_bucket_size - n_active_nodes)]
             })
 
         if is_training_data:
@@ -149,6 +151,34 @@ class DenseGGNNChemModel(ChemModel):
 
         return (bucketed, bucket_sizes, bucket_at_step)
 
+    def pad_annotations(self, annotations):
+        return  np.pad(annotations,
+                       pad_width=[[0, 0], [0, 0], [0, self.params['hidden_size'] - self.annotation_size]],
+                       mode='constant')
+
+
+    def make_batch(self, elements):
+        batch_data = {'adj_mat': [], 'init': [], 'labels': [], 'node_mask': [], 'task_masks': []}
+        for d in elements:
+            batch_data['adj_mat'].append(d['adj_mat'])
+            batch_data['init'].append(d['init'])
+            batch_data['node_mask'].append(d['mask'])
+
+            target_task_values = []
+            target_task_mask = []
+            for target_val in d['labels']:
+                if target_val is None:  # This is one of the examples we didn't sample...
+                    target_task_values.append(0.)
+                    target_task_mask.append(0.)
+                else:
+                    target_task_values.append(target_val)
+                    target_task_mask.append(1.)
+            batch_data['labels'].append(target_task_values)
+            batch_data['task_masks'].append(target_task_mask)
+
+        return batch_data
+
+
     def make_minibatch_iterator(self, data, is_training: bool):
         (bucketed, bucket_sizes, bucket_at_step) = data
         if is_training:
@@ -163,28 +193,12 @@ class DenseGGNNChemModel(ChemModel):
             start_idx = bucket_counters[bucket] * self.params['batch_size']
             end_idx = (bucket_counters[bucket] + 1) * self.params['batch_size']
             elements = bucketed[bucket][start_idx:end_idx]
-            batch_data = {'adj_mat': [], 'init': [], 'labels': [], 'task_masks': []}
-            for d in elements:
-                batch_data['adj_mat'].append(d['adj_mat'])
-                batch_data['init'].append(d['init'])
-
-                target_task_values = []
-                target_task_mask = []
-                for target_val in d['labels']:
-                    if target_val is None:  # This is one of the examples we didn't sample...
-                        target_task_values.append(0.)
-                        target_task_mask.append(0.)
-                    else:
-                        target_task_values.append(target_val)
-                        target_task_mask.append(1.)
-                batch_data['labels'].append(target_task_values)
-                batch_data['task_masks'].append(target_task_mask)
+            batch_data = self.make_batch(elements)
 
             num_graphs = len(batch_data['init'])
             initial_representations = batch_data['init']
-            initial_representations = np.pad(initial_representations,
-                                             pad_width=[[0, 0], [0, 0], [0, self.params['hidden_size'] - self.annotation_size]],
-                                             mode='constant')
+            initial_representations = self.pad_annotations(initial_representations)
+
             batch_feed_dict = {
                 self.placeholders['initial_node_representation']: initial_representations,
                 self.placeholders['target_values']: np.transpose(batch_data['labels'], axes=[1,0]),
@@ -192,6 +206,7 @@ class DenseGGNNChemModel(ChemModel):
                 self.placeholders['num_graphs']: num_graphs,
                 self.placeholders['num_vertices']: bucket_sizes[bucket],
                 self.placeholders['adjacency_matrix']: batch_data['adj_mat'],
+                self.placeholders['node_mask']: batch_data['node_mask'],
                 self.placeholders['graph_state_keep_prob']: dropout_keep_prob,
             }
 
@@ -199,12 +214,52 @@ class DenseGGNNChemModel(ChemModel):
 
             yield batch_feed_dict
 
+    def evaluate_one_batch(self, initial_node_representations, adjacency_matrices, node_masks=None):
+        num_vertices = len(initial_node_representations[0])
+        if node_masks is None:
+            node_masks = []
+            for r in initial_node_representations:
+                node_masks.append([1. for _ in r] + [0. for _ in range(num_vertices - len(r))])
+        batch_feed_dict = {
+            self.placeholders['initial_node_representation']: self.pad_annotations(initial_node_representations),
+            self.placeholders['num_graphs']: len(initial_node_representations),
+            self.placeholders['num_vertices']: len(initial_node_representations[0]),
+            self.placeholders['adjacency_matrix']: adjacency_matrices,
+            self.placeholders['node_mask']: node_masks,
+            self.placeholders['graph_state_keep_prob']: 1.0,
+            self.placeholders['out_layer_dropout_keep_prob']: 1.0,
+        }
+
+        fetch_list = self.output
+        result = self.sess.run(fetch_list, feed_dict=batch_feed_dict)
+        return result
+
+    def example_evaluation(self):
+        ''' Demonstration of what test-time code would look like
+        we query the model with the first n_example_molecules from the validation file
+        '''
+        n_example_molecules = 10
+        with open('molecules_valid.json', 'r') as valid_file:
+            example_molecules = json.load(valid_file)[:n_example_molecules]
+        example_molecules, _, _ = self.process_raw_graphs(example_molecules, 
+            is_training_data=False, bucket_sizes=np.array([29]))
+        batch_data = self.make_batch(example_molecules[0])
+        print(self.evaluate_one_batch(batch_data['init'], batch_data['adj_mat']))
+
+        
+
+
 
 def main():
     args = docopt(__doc__)
     try:
         model = DenseGGNNChemModel(args)
-        model.train()
+
+        evaluation = False
+        if evaluation:
+            model.example_evaluation()
+        else:
+            model.train()
     except:
         typ, value, tb = sys.exc_info()
         traceback.print_exc()
